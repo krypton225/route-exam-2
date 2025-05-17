6833401482174349d90236ccf8b321132b4d8c6f
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-05-17 16:42:33 +0300

    add JSDoc comments for run & hide functions in Loader module

--- js/loader.js
+++ js/loader.js
@@ -3,10 +3,16 @@
 const Loader = (function () {
     const loader = document.getElementById("loader");
 
+    /**
+     * Displays the loader by removing the "d-none" class from its element.
+     */
     function run() {
         loader.classList.remove("d-none");
     }
 
+    /**
+     * Hides the loader by adding the "d-none" class to its element.
+     */
     function hide() {
         loader.classList.add("d-none");
     }
